28cf8a676c664ed904019c78c0307c7dc9f876d5
ChoiHeynho <chh0006@gmail.com>
2022-08-03 00:55:31 +0900

    1000practice resolved

--- javascript/Add.js
+++ javascript/Add.js
@@ -1,26 +1,58 @@
 //1000번 문제
 
+
+
 const readline = require('readline');
-//const {stdin:input, stdout:output} = require("process");
 
 //create Interface함수를 이용해 readline객체 생성
 const rl = readline.createInterface({input: process.stdin,output: process.stdout,});
 
 
+let A, B;
+
+if((A==undefined) && (B==undefined)){
+    input();//input과 ouput시점이 바뀌는건 콜백함수 때문인가?
+    rl.on('close',function(){
+        output();
+        process.exit();
+    });
+}
 
 
-//구조 분해 할당(Destructuring Assignment)을 사용
-rl.question("A와 B를 입력하세요 : ", function(operands){
-    let[A,B] = operands.split(" ").map(element => parseInt(element));
+
+
+//값 입력
+function input(){
+    process.stdout.write("A와 B의 값을 각각 입력하세요.(단, 0 < A, B < 10) : ");
+    rl.on('line',function(operands){
+            [A,B] = operands.split(" ").map(element => parseInt(element));
+            if(check()){
+                rl.close();
+            }else{
+                process.stdout.write("입력 조건이 만족되지 않았습니다.다시 입력하세요.. : ");
+            }
+    });
+}
+
+
+
+//입력값 체크
+function check(){
+    if((0 < A) && (B < 10)){
+        return true;
+    }
+    return false;
+}
+
+//결과 출력
+function output(){
     console.log(A+B);
-    rl.close();
-});
+}
 
 
-//참고 링크
-//구조 분해 할당
-//https://chanhuiseok.github.io/posts/js-10/
 
-
-//콘솔 입력 받기
-//https://sebhastian.com/javascript-console-input/
+/*
+참고 링크
+https://chanhuiseok.github.io/posts/js-10/ : 구조 분해 할당
+https://sebhastian.com/javascript-console-input/ : 콘솔 입력 받기
+*/
